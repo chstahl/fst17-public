e45ee56a32c9de77467bca18be9ab135e93c5ec0
chstahl <chstahl@users.noreply.github.com>
2017-11-09 13:36:13 +0100

    updated for Sonar

--- sonar-maven.sh
+++ sonar-maven.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn clean install sonar:sonar -Dsonar.host.url=https:/sonarqube.com -Dsonar.organization=sbaltes-github -Dsonar.login=SONARCLOUD_AUTHENTICATION_TOKEN
+mvn clean install sonar:sonar -Dsonar.host.url=https:/sonarqube.com -Dsonar.organization=chstahl-github -Dsonar.login=94598a5f8baa4c0fad6423958e7a49620a183ff0
